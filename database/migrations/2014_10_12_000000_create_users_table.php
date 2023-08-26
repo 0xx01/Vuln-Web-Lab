d6497dfc252c94a25bf95e9071c8417cac1a86d3
Eslam Omar <130947610+0xx01@users.noreply.github.com>
2023-08-26 20:01:52 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->integer('role');
+            $table->integer('role')->nullable();
             $table->timestamps();
             $table->
         });
